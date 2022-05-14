67d77f62c703c06717c6c2de08c9ace1c2751c5e
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2022-05-14 19:16:02 +0100

    rps.js function updated

--- rps.js
+++ rps.js
@@ -1,13 +1,11 @@
 let rng = Math.floor(Math.random() * 3 + 1);
 
 function random(rng) {
-  if (rng > 2) {
+  if (rng === 3) {
     console.log(`rock`);
-  }
-  if ((rng = 2)) {
+  } else if (rng === 2) {
     console.log(`paper`);
-  }
-  if (rng < 2) {
+  } else if (rng === 1) {
     console.log(`scissors`);
   }
 }
