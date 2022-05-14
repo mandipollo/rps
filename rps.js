9d9654a62938b180f9220234c4bc3d2669f1556e
mandip gurung <78259561+mandipollo@users.noreply.github.com>
2022-05-14 20:01:31 +0100

    reworked the computer function

--- rps.js
+++ rps.js
@@ -1,13 +1,14 @@
-let rng = Math.floor(Math.random() * 3 + 1);
+//
 
-function random(rng) {
-  if (rng === 3) {
-    console.log(`rock`);
-  } else if (rng === 2) {
-    console.log(`paper`);
-  } else if (rng === 1) {
-    console.log(`scissors`);
-  }
+// function for computer selection
+function computerSelection() {
+  let randomValue = Math.floor(Math.random() * 3 + 1);
+
+  if (randomValue == 1) {
+    return `ROCKS`;
+  } else if (randomValue == 2) {
+    return `PAPERS`;
+  } else return `SCISSORS`;
 }
-console.log(rng);
-random(rng);
+
+console.log(computerSelection());
